c97babff4b9de59c4c8eec0b83d8269a18c2d380
Nixkoo1 <125210290+Nixkoo1@users.noreply.github.com>
2023-02-15 20:50:33 -0500

    first commit

---
+++ prework_1-5.py
@@ -0,0 +1,37 @@
+# Question 1:
+# Write a function to print " hello_USERNAME!" Username is the input of the function.
+
+def hello_name(user_name):
+    hello_USERNAME!
+
+
+# Question 2 :
+# Write a python function, first_odds that prints the old numbersfrom 1-100 and returns nothing
+def first_odds():
+    def first_odds():
+    print("first_odds")
+
+
+# Question 3:
+# Please write a Python function,max_num_in_list to return the max number of a given list. The first line of the code has been defined below
+
+def max_num_in_list(a_list):
+    None
+
+
+# Question 4:
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false)
+def is_leap_year(a_year):
+    def is_leap():
+        leap = False
+        if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
+            leap = True
+            return leap
+
+
+
+# Question 5:
+# Write a function to check to see if all numbers in list are consecutive numbers. For example,[2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+def is_consecutive(a_list):
+    False
+    
