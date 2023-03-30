b682f86a47cd19792b41a1eada959eb28495bc01
Nixkoo1 <125210290+Nixkoo1@users.noreply.github.com>
2023-03-30 13:32:16 -0400

    Add files via upload

--- prework_1-5.py
+++ prework_1-5.py
@@ -2,36 +2,72 @@
 # Write a function to print " hello_USERNAME!" Username is the input of the function.
 
 def hello_name(user_name):
-    hello_USERNAME!
+    
+    print("hello" + user_name.upper() + "!")
+
+
+hello_name('Nick')
 
 
 # Question 2 :
 # Write a python function, first_odds that prints the old numbersfrom 1-100 and returns nothing
+
+
 def first_odds():
-    def first_odds():
-    print("first_odds")
+    for i in range(1, 101): 
+        if i % 2 !=0:
+            print(i)
+
+
+first_odds()
 
 
 # Question 3:
 # Please write a Python function,max_num_in_list to return the max number of a given list. The first line of the code has been defined below
 
+
 def max_num_in_list(a_list):
-    None
+    maximum = a_list[0]
+    for i in range(len(a_list)):
+        if a_list[i] > maximum:
+            maximum = a_list[i]
+    return maximum
+
+
+a_list = [10,20,30,40,50]
+print(max_num_in_list(a_list))
 
 
 # Question 4:
 # Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false)
+
 def is_leap_year(a_year):
-    def is_leap():
-        leap = False
-        if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
-            leap = True
-            return leap
+    
+    if a_year % 4 == 0:
+        print("True")
+
+    elif a_year % 100 == 0:
+        print("False")
+        
+    elif a_year % 400 == 0:
+        print("True")
+
+    return a_year
 
 
+is_leap_year(2016)
 
 # Question 5:
 # Write a function to check to see if all numbers in list are consecutive numbers. For example,[2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+
 def is_consecutive(a_list):
-    False
+    return sorted(a_list) == list(range(min(a_list), max(a_list)+1))
+
+
+a_list = [2,3,4,5,6,7]
+print(is_consecutive(a_list))
+a_list = [1,2,4,5]
+print(is_consecutive(a_list))
+
     
